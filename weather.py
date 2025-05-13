5ef26a3c835d0ed165a468f8cc1322656b8663b5
Fionna Immaculate <131240109+fioimma-23@users.noreply.github.com>
2025-05-13 16:55:49 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,38 @@
+import autogen
+import requests
+
+class WeatherAgent(autogen.ConversableAgent):
+    def get_weather(self, city, api_key):
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+        response = requests.get(url)
+        data = response.json()
+
+        if response.status_code == 200:
+            return data
+        else:
+            return {"error"}
+
+class DecisionAgent(autogen.ConversableAgent):
+    def fetch_weather(self, weather_data):
+        if 'error' in weather_data:
+            return "Data unavailable"
+        
+        main = weather_data['weather'][0]['main']
+        temp = weather_data['main']['temp']
+        result = f"Weather: {main}, Temp: {temp}C"
+
+        return result
+    
+weather_agent = WeatherAgent(name="WeatherAgent")
+decision_agent = DecisionAgent(name="DecisionAgent")
+
+def weather_detection(city, api_key):
+    weather_data = weather_agent.get_weather(city, api_key)
+
+    result = decision_agent.fetch_weather(weather_data)
+    print(result)
+
+if __name__ == "__main__":
+    API_KEY = "f16e046fa64dee40b1abcbb3fdbc2d22"
+    city = input("Enter the city: ")
+    weather_detection(city, API_KEY)
